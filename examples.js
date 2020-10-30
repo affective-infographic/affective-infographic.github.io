614a5a01507119caad9b40a70ce19926084b6e6a
Olivia Lan <568854812@qq.com>
2020-10-30 18:13:10 +0800

    update

--- examples.js
+++ examples.js
@@ -1,94 +1,138 @@
 examples = {
     "H1-1-1": {
         "src": "pic649.png",
+        "title": "Shifting Gears: Visualizing Cycle Rides",
+        "link": "https://www.informationisbeautifulawards.com/showcase/3204-shifting-gears-visualizing-cycle-rides",
         "comment": "It seems like it's overcomplicating information that doesn't need to be that complicated. This definitely only makes me overwhelmed and annoyed.",
     },
     "H1-1-2": {
         "src": "pic163.jpeg",
+        "title": "The cost of living in America",
+        "link": "https://www.informationisbeautifulawards.com/showcase/791-the-cost-of-living-in-america",
         "comment": "There's a lot going on visually and I have no idea how it's all connected. It's basically a big explosion but the explosion is data.",
     },
     "H1-1-3": {
         "src": "pic889.jpeg",
+        "title": "Following Twitter",
+        "link": "https://visual.ly/community/Infographics/social-media/following-twitter",
         "comment": "I am glad for this avenue of information gathering offered to the people.",
         "src2": "pic643.png"
     },
     "H1-2-1": {
         "src": "pic563.png",
+        "title": "The Insane Choices You Face At The Drug Store",
+        "link": "https://visual.ly/community/Infographics/health/insane-choices-you-face-drug-store",
         "comment": "its hard to see any info on the infographics",
     },
     "H1-2-2": {
         "src": "pic246.png",
+        "title": "The Universe of Italian Football",
+        "link": "https://www.informationisbeautifulawards.com/showcase/1105-the-universe-of-italian-football",
         "comment": "I think the secondary text color needs a bit more contrast, and the color of the numbers needs to be a bit brighter. Just looks a bit dull overall and hard to read."
     },
     "H1-2-3": {
         "src": "pic11.jpeg",
+        "title": "Birth Of Earthquake",
+        "link": "https://www.informationisbeautifulawards.com/showcase/4489-birth-of-earthquake",
         "comment": "Typo on first word - Brith - makes it seem unprofessional, which is a shame",
     },
     "H1-3-1": {
         "src": "pic849.png",
+        "title": "The X-Men Family",
+        "link": "https://visual.ly/community/Infographics/entertainment/x-men-family",
         "comment": "I'm completely annoyed that they didn't take the time to label the pictures, and then told us to go find the information ourselves down at the bottom. If you can take the time to create this, you can take the extra time to label it.",
     },
     "H1-3-2": {
         "src": "pic645.png",
+        "title": 'On "Geek" vs. "Nerd"',
+        "link": "https://www.informationisbeautifulawards.com/showcase/399-on-geek-vs-nerd",
         "comment": "There is no title for the infographic or no summary. I don't understand what this is about. Trend words?  Twitter hashtags?",
     },
     "H1-3-3": {
         "src": "pic200.jpeg",
+        "title": "Patent Wars",
+        "link": "https://visual.ly/community/infographic/business/patent-wars",
         "comment": "This is a confusing visual. I think it needs some more explanation on how to read it, I don't quite understand what is going on.",
     },
     "H1-4-1": {
         "src": "pic115.png",
+        "title": "LEFT VS RIGHT",
+        "link": "https://visual.ly/community/infographic/politics/left-vs-right",
         "comment": "I love how both of the political parties were shown in this graphic. It seems very well balanced and gives a non-biased and equal look at both.",
         "src2": "pic680.png"
     },
     "H1-4-2": {
         "src": "pic447.jpeg",
+        "title": "HEROINE - Gender Differences in Mobile Gaming",
+        "link": "https://www.informationisbeautifulawards.com/showcase/3309-heroine-gender-differences-in-mobile-gaming",
         "comment": "I was very content with the information and sources provided in this infographic and felt satisfied by the information provided knowing it was backed up by analytics.",
         "src2": "pic448.png",
         "comment2": " I was also very annoyed by the lack of sources and context to explain why any of the graphs were important and what exactly it was trying to prove."
     },
     "H2-1-1": {
         "src": "pic186.jpeg",
+        "title": "This is not a graph",
+        "link": "https://www.informationisbeautifulawards.com/showcase/2564-this-is-not-a-graph-ceci-n-est-pas-un-graph",
         "comment": "I am excited to see that the chart was drawn in chocolate. Really cool and fun graphic to take in. I really enjoyed this."
     },
     "H2-1-2": {
         "src": "pic315.jpeg",
+        "title": "10 Signs You're Reading a Gothic Novel",
+        "link": "https://www.informationisbeautifulawards.com/showcase/554-10-signs-you-re-reading-a-gothic-novel",
         "comment": "This has a horror vibe to it. I found the blood and bloody footprints to be somewhat shocking."
     },
     "H2-1-3": {
         "src": "pic513.png",
+        "title": "Trees Of New York City",
+        "link": "https://www.informationisbeautifulawards.com/showcase/3933-trees-of-new-york-city",
         "comment": "The color used was very bright and reflected the overall positive feeling gained by reading the presented information.",
     },
     "H2-1-4": {
         "src": "pic355.jpeg",
+        "title": "How I Met Your Mother Through Your Grandparents",
+        "link": "https://www.informationisbeautifulawards.com/showcase/3138-how-i-met-your-mother-through-your-grandparents",
         "comment": "The infographic has a warm and friendly personality and displays things in a interesting manner.",
     },
     "H2-1-5": {
         "src": "pic314.png",
-        "comment": "There are a lot of colors and activity on the page. It just gives me an excited kind of vibe.",
+        "title": "The Internet of Things in Four Fruits",
+        "link": "https://www.informationisbeautifulawards.com/showcase/2283-the-internet-of-things-in-four-fruits",
+        "comment": "There are a lot of colors and activity on the page. It just gives me cheering vibe.",
     },
     "H2-1-6": {
         "src": "pic311.png",
+        "title": "One LGBT+ person is killed every day in Brazil",
+        "link": "https://www.informationisbeautifulawards.com/showcase/3176-one-lgbt-person-is-killed-every-day-in-brazil",
         "comment": "The bloody red makes me really pay attention to the facts, it makes me feel shocked."
     },
     "H2-2-1": {
         "src": "pic153.jpeg",
+        "title": "Did you know some countries require you to take a vacation?",
+        "link": "https://visual.ly/community/infographic/business/did-you-know-some-countries-require-you-take-vacation",
         "comment": "The infographic is visually very engaging and easy to absorb the key information. The topic is frustrating to a US citizen.",
     },
     "H2-2-2": {
         "src": "pic270.png",
+        "title": "Details On Fast Food",
+        "link": "https://www.informationisbeautifulawards.com/showcase/4356-details-on-fast-food",
         "comment": "The title is designed as if someone is yelling to me. The information itself is fairly shocking and is important for people to know about."
     },
     "H2-2-3": {
         "src": "pic642.jpeg",
+        "title": "modern DIEt",
+        "link": "https://www.informationisbeautifulawards.com/showcase/3215-modern-diet",
         "comment": "First thing I noticed was the DIE in the title; pretty shocking and grab my attention."
     },
     "H2-3-1": {
         "src": "pic848.png",
+        "title": "How Long Do Animals Live?",
+        "link": "https://visual.ly/community/infographic/animals/how-long-do-animals-live",
         "comment": "What a clever infographic, presenting the lifespans of animals as part of a continuous line. Wow.",
     },
     "H2-3-2": {
         "src": "pic791.jpeg",
+        "title": "Web Designers vs. Web Developers",
+        "link": "https://visual.ly/community/Infographics/computers/web-designers-vs-web-developers",
         "comment": "This is a cute infograph about the differences of people at different jobs.",
     }
 }
