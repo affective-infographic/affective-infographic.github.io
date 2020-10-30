713abecd222e77768bb6d04ddbd7e0e8df3742bd
Olivia Lan <568854812@qq.com>
2020-10-30 17:24:59 +0800

    update

--- examples.js
+++ examples.js
@@ -1,68 +1,94 @@
 examples = {
     "H1-1-1": {
-        "src": "pic649.png"
+        "src": "pic649.png",
+        "comment": "It seems like it's overcomplicating information that doesn't need to be that complicated. This definitely only makes me overwhelmed and annoyed.",
     },
     "H1-1-2": {
-        "src": "pic163.png"
+        "src": "pic163.jpeg",
+        "comment": "There's a lot going on visually and I have no idea how it's all connected. It's basically a big explosion but the explosion is data.",
     },
     "H1-1-3": {
-        "src": "pic643.png"
+        "src": "pic889.jpeg",
+        "comment": "I am glad for this avenue of information gathering offered to the people.",
+        "src2": "pic643.png"
     },
     "H1-2-1": {
-        "src": "pic563.png"
+        "src": "pic563.png",
+        "comment": "its hard to see any info on the infographics",
     },
     "H1-2-2": {
-        "src": "pic741.png"
+        "src": "pic246.png",
+        "comment": "I think the secondary text color needs a bit more contrast, and the color of the numbers needs to be a bit brighter. Just looks a bit dull overall and hard to read."
     },
     "H1-2-3": {
-        "src": ""
+        "src": "pic11.jpeg",
+        "comment": "Typo on first word - Brith - makes it seem unprofessional, which is a shame",
     },
     "H1-3-1": {
-        "src": ""
+        "src": "pic849.png",
+        "comment": "I'm completely annoyed that they didn't take the time to label the pictures, and then told us to go find the information ourselves down at the bottom. If you can take the time to create this, you can take the extra time to label it.",
     },
     "H1-3-2": {
-        "src": ""
+        "src": "pic645.png",
+        "comment": "There is no title for the infographic or no summary. I don't understand what this is about. Trend words?  Twitter hashtags?",
     },
     "H1-3-3": {
-        "src": ""
+        "src": "pic200.jpeg",
+        "comment": "This is a confusing visual. I think it needs some more explanation on how to read it, I don't quite understand what is going on.",
     },
     "H1-4-1": {
-        "src": ""
+        "src": "pic115.png",
+        "comment": "I love how both of the political parties were shown in this graphic. It seems very well balanced and gives a non-biased and equal look at both.",
+        "src2": "pic680.png"
     },
     "H1-4-2": {
-        "src": ""
+        "src": "pic447.jpeg",
+        "comment": "I was very content with the information and sources provided in this infographic and felt satisfied by the information provided knowing it was backed up by analytics.",
+        "src2": "pic448.png",
+        "comment2": " I was also very annoyed by the lack of sources and context to explain why any of the graphs were important and what exactly it was trying to prove."
     },
     "H2-1-1": {
-        "src": "pic186.png"
+        "src": "pic186.jpeg",
+        "comment": "I am excited to see that the chart was drawn in chocolate. Really cool and fun graphic to take in. I really enjoyed this."
     },
     "H2-1-2": {
-        "src": "pic516.png"
+        "src": "pic315.jpeg",
+        "comment": "This has a horror vibe to it. I found the blood and bloody footprints to be somewhat shocking."
     },
     "H2-1-3": {
-        "src": "pic466.png"
+        "src": "pic513.png",
+        "comment": "The color used was very bright and reflected the overall positive feeling gained by reading the presented information.",
     },
     "H2-1-4": {
-        "src": "pic216.png"
+        "src": "pic355.jpeg",
+        "comment": "The infographic has a warm and friendly personality and displays things in a interesting manner.",
     },
     "H2-1-5": {
-        "src": "pic314.png"
+        "src": "pic314.png",
+        "comment": "There are a lot of colors and activity on the page. It just gives me an excited kind of vibe.",
     },
     "H2-1-6": {
-        "src": "pic311.png"
+        "src": "pic311.png",
+        "comment": "The bloody red makes me really pay attention to the facts, it makes me feel shocked."
     },
     "H2-2-1": {
-        "src": "pic153.png"
+        "src": "pic153.jpeg",
+        "comment": "The infographic is visually very engaging and easy to absorb the key information. The topic is frustrating to a US citizen.",
     },
     "H2-2-2": {
-        "src": "pic270.png"
+        "src": "pic270.png",
+        "comment": "The title is designed as if someone is yelling to me. The information itself is fairly shocking and is important for people to know about."
     },
     "H2-2-3": {
-        "src": "pic642.png"
+        "src": "pic642.jpeg",
+        "comment": "First thing I noticed was the DIE in the title; pretty shocking and grab my attention."
     },
     "H2-3-1": {
-        "src": "pic758.png"
+        "src": "pic848.png",
+        "comment": "What a clever infographic, presenting the lifespans of animals as part of a continuous line. Wow.",
     },
     "H2-3-2": {
-        "src": "pic791.png"
+        "src": "pic791.jpeg",
+        "comment": "This is a cute infograph about the differences of people at different jobs.",
     }
 }
